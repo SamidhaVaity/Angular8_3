89931072ba915d45037af3adbb37b50d71fc241d
Samidha Vaity <samidhavaity@gmail.com>
2023-12-31 18:21:09 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+import { DemoComponent } from './demo/demo.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration()
